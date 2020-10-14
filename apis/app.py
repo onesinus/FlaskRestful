62c5d16a7f4368831f3dbcc45f4d439c7c596a8c
kodekito <kodekito@gmail.com>
2020-10-14 14:08:17 +0700

    ...

--- apis/app.py
+++ apis/app.py
@@ -1,7 +1,6 @@
 from apis import create_app
 from apis.db import init_app
 
-if __name__ == '__main__':
-    app = create_app()
-    init_app(app)
-    app.run()
+app = create_app()
+init_app(app)
+app.run()
