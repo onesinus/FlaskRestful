fad7907b0cf8ba41814b2ed9a15a1c747078a26e
kodekito <kodekito@gmail.com>
2020-10-14 14:22:11 +0700

    ...

--- apis/__init__.py
+++ apis/__init__.py
@@ -4,41 +4,33 @@ import os
 
 from flask import Flask, g
 
-def app(test_config = None):
-    app = Flask(__name__, instance_relative_config = True)
-    app.config.from_mapping(
-        SECRET_KEY='dev',
-        DATABASE=os.path.join(app.instance_path, 'api.sqlite')
-    )
+app = Flask(__name__, instance_relative_config = True)
+app.config.from_mapping(
+    SECRET_KEY='dev',
+    DATABASE=os.path.join(app.instance_path, 'api.sqlite')
+)
 
-    if test_config is None:
-        app.config.from_pyfile('config.py', silent=True)
-    else:
-        app.config.from_mapping(test_config)
+# if test_config is None:
+#     app.config.from_pyfile('config.py', silent=True)
+# else:
+#     app.config.from_mapping(test_config)
 
-    try:
-        os.makedirs(app.instance_path)
-    except OSError:
-        pass
-    
-    @app.route('/hello')
-    def hello():
-        return {
-            "loggedInUser": g.user
-        }
+try:
+    os.makedirs(app.instance_path)
+except OSError:
+    pass
 
-    from . import db
-    db.init_app(app);
+@app.route('/hello')
+def hello():
+    return {
+        "loggedInUser": g.user
+    }
 
-    from . import auth
-    from . import blog
-    
-    app.register_blueprint(auth.bp)
-    app.register_blueprint(blog.bp)
+from . import db
+db.init_app(app);
 
-    return app
+from . import auth
+from . import blog
 
-# if __name__ == '__main__':
-#     app = create_app()
-#     init_app(app)
-#     app.run()
+app.register_blueprint(auth.bp)
+app.register_blueprint(blog.bp)
